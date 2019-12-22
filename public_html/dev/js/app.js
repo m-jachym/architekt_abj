0abb4abd1d63228e4105931802a818057bf38e1a
evr <judym.marek@gmail.com>
2019-12-23 00:25:57 +0100

    up

--- public_html/dev/js/app.js
+++ public_html/dev/js/app.js
@@ -5,7 +5,7 @@ $(window).on('scroll', function() {
     else {
         $('.wrapper-menu').removeClass('menu-thin');
     } 
-})
+});
 
 $(document).ready(function(){
     $('.hamburger').click(function(){
@@ -13,4 +13,20 @@ $(document).ready(function(){
         $('.line').toggleClass('line-hide');
         $('.nav-ul').toggleClass('nav-display');     
     })
-})
+});
+
+// tabs
+let tabBody = $('.tab-body');
+let circleBtn = $('.circle-btn');
+
+$('.tab-header').click(function(){
+
+    tabBody.hasClass('tab-body-visible') ?
+    $('.tab-body').removeClass('tab-body-visible')
+    :
+    $('.tab-body').addClass('tab-body-visible');
+    circleBtn.hasClass('circle-btn-rotate') ?
+    $('.circle-btn').removeClass('circle-btn-rotate')
+    :
+    $('.circle-btn').addClass('circle-btn-rotate');
+});
