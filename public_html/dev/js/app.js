971da34bfea6de7e3bb5f34a3319aa0aa03ac806
evr <judym.marek@gmail.com>
2020-01-19 21:20:46 +0100

    up

--- public_html/dev/js/app.js
+++ public_html/dev/js/app.js
@@ -1,32 +1,48 @@
-$(window).on('scroll', function() {
-    if($(window).scrollTop()) {
+$(window).on('scroll', function () {
+    if ($(window).scrollTop()) {
         $('.wrapper-menu').addClass('menu-thin');
-    }
-    else {
+    } else {
         $('.wrapper-menu').removeClass('menu-thin');
-    } 
+    }
 });
 
-$(document).ready(function(){
-    $('.hamburger').click(function(){
+$(document).ready(function () {
+    $('.hamburger').click(function () {
         $('.hamburger').toggleClass('hamburger-clicked');
         $('.line').toggleClass('line-hide');
-        $('.nav-ul').toggleClass('nav-display');     
+        $('.nav-ul').toggleClass('nav-display');
     })
 });
 
 // tabs
-let tabBody = $('.tab-body');
-let circleBtn = $('.circle-btn');
 
-$('.tab-header').click(function(){
+$('.tab-header').click(function () {
+    var tab_id = $(this).attr('data-tab');
 
-    tabBody.hasClass('tab-body-visible') ?
-    $('.tab-body').removeClass('tab-body-visible')
-    :
-    $('.tab-body').addClass('tab-body-visible');
-    circleBtn.hasClass('circle-btn-rotate') ?
-    $('.circle-btn').removeClass('circle-btn-rotate')
-    :
-    $('.circle-btn').addClass('circle-btn-rotate');
+    $("#" + tab_id).hasClass('tab-body-visible') ?
+
+        $("#" + tab_id).removeClass('tab-body-visible')
+        :
+        $("#" + tab_id).addClass('tab-body-visible');
+
+    $("#" + tab_id).hasClass('tab-body-visible') ?
+
+        $("#btn" + tab_id).addClass('circle-btn-rotate')
+        :
+        $("#btn" + tab_id).removeClass('circle-btn-rotate');
+
 });
+
+$('.kurwamax').click(function () {
+    var tab_id = $(this).attr('data-tab');
+    $("#" + tab_id).removeClass('tab-body-visible');
+    $("#btn" + tab_id).removeClass('circle-btn-rotate');
+});
+
+$("#gmap_canvas")
+    .gmap3({action: 'init',
+        options:{
+            mapTypeControl:false,//hide mapTypeControl
+            streetViewControl: false//set it to false to hide the "man"
+        }
+    });
